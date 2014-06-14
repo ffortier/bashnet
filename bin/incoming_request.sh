0685591fe39ee1f8398d8866ef0e0ec517534b78
Francis Fortier <francis.fortier@gmail.com>
2014-06-14 11:18:02 -0400

    use tcpserver if present

---
+++ bin/incoming_request.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+CWD=$(pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOG_FILE='/var/bashnet/application.log'
+
+touch $LOG_FILE
+
+HANDLERS='-e "s/.*/file_handler &/"'
+
+for f in $DIR/handlers/*; do source $f; done
+
+function incoming_request {
+    local IN
+    local METHOD
+    local URL
+    local VERSION
+
+    read METHOD URL VERSION
+
+    log_info "Incomming request method=$METHOD path=$URL version=$VERSION"
+
+    local HEADERS=$(read_headers)
+    local HANDLER=$(eval "sed $HANDLERS <<< '$URL'")
+
+    if [[ -n "$HANDLER" ]]; then
+        $HANDLER "$METHOD" "$URL" "$HEADERS"
+    else
+        error_handler 500 "No handler found"
+    fi
+}
+
+function read_headers {
+    while read -r IN; do
+        IN=$(echo $IN | tr -d '\r' | tr -d '\n')
+        if [ -z "$IN" ]; then
+            log_info "End of headers"
+            break
+        else
+            log_debug "$IN"
+            echo "$IN"
+        fi
+    done
+}
+
+function log_info {
+    echo "INFO: $1" >> $LOG_FILE
+}
+
+function log_debug {
+    echo "DEBUG: $1" >> $LOG_FILE
+}
+
+function log_error {
+    echo "ERROR: $1" >> $LOG_FILE
+}
+
+incoming_request
--- bin/server.sh
+++ bin/server.sh
@@ -1,65 +1,20 @@
 #!/bin/bash
-CWD=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LOG_FILE='/var/bashnet/application.log'
-
-touch $LOG_FILE
-
-HANDLERS='-e "s/.*/file_handler &/"'
-
-for f in $DIR/handlers/*; do source $f; done
 
 function main {
-    while true; do
-        coproc incoming_request
-        nc -l -p 8080 <&"${COPROC[0]}" >&"${COPROC[1]}"
-        wait $!
-    done
-}
-
-function incoming_request {
-    local IN
-    local METHOD
-    local URL
-    local VERSION
-
-    read METHOD URL VERSION
-
-    log_info "Incomming request method=$METHOD path=$URL version=$VERSION"
-
-    local HEADERS=$(read_headers)
-    local HANDLER=$(eval "sed $HANDLERS <<< '$URL'")
-
-    if [[ -n "$HANDLER" ]]; then
-        $HANDLER "$METHOD" "$URL" "$HEADERS"
+    if type tcpserver > /dev/null 2>&1; then
+        tcpserver 0 8080 sh -c $DIR/incoming_request.sh
     else
-        error_handler 500 "No handler found"
+        while true; do
+            coproc exec_incoming_request
+            nc -l -p 8080 <&"${COPROC[0]}" >&"${COPROC[1]}"
+            wait $!
+        done
     fi
 }
 
-function read_headers {
-    while read -r IN; do
-        IN=$(echo $IN | tr -d '\r' | tr -d '\n')
-        if [ -z "$IN" ]; then
-            log_info "End of headers"
-            break
-        else
-            log_debug "$IN"
-            echo "$IN"
-        fi
-    done
-}
-
-function log_info {
-    echo "INFO: $1" >> $LOG_FILE
-}
-
-function log_debug {
-    echo "DEBUG: $1" >> $LOG_FILE
-}
-
-function log_error {
-    echo "ERROR: $1" >> $LOG_FILE
+function exec_incoming_request {
+    $DIR/incoming_request.sh
 }
 
 main
